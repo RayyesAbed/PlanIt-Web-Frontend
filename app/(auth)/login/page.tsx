5ed03866045ae344e24cee3895306cdbd8375c1a
RayyesAbed <abedray1567@gmail.com>
2025-08-17 13:26:31 +0200

    Separated the login and page components in their files, and wrapped the login in the page component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,28 +1,11 @@
-"use client";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import RandomLoginModel from "./_components/RandomLoginModel";
-import Logo from "@/app/_components/Logo";
+import { Metadata } from "next";
+import LoginPageClient from "./LoginPageClient";
 
-const LoginPage = () => {
-  return (
-    <main className="flex items-center h-screen gap-5 mx-5">
-      <section className="flex flex-col items-center flex-1/2 h-4/5">
-        <Logo theme="light" />
-      </section>
-      <section className="flex-1/2 h-4/5">
-        <Canvas
-          camera={{ position: [0, 2, 5], fov: 50 }}
-          className="bg-gray-200 rounded-4xl"
-        >
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[10, 10, 5]} intensity={3} />
-          <RandomLoginModel />
-          <OrbitControls minDistance={90} maxDistance={200} />
-        </Canvas>
-      </section>
-    </main>
-  );
+export const metadata: Metadata = {
+  title: "Login | PlanIt",
+  description: "Login to continue your journey",
 };
 
-export default LoginPage;
+export default function Page() {
+  return <LoginPageClient />;
+}
