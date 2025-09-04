2c02662eb26e79273b3fba9d4ad91cfb6ee0280a
RayyesAbed <abedray1567@gmail.com>
2025-09-04 18:55:19 +0200

    Added Register component to Page server component, and used Metadata, hence closing ticket [PI2-1]

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+import RegisterPageClient from "./RegisterPageClient";
+
+export const metadata: Metadata = {
+  title: "Register | PlanIt",
+  description: "Create a new account to start your journey",
+};
+
+export default function Page() {
+  return <RegisterPageClient />;
+}
