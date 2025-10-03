4163d91f766ee279a692b43a932896000c4711bf
RayyesAbed <abedray1567@gmail.com>
2025-10-03 15:10:46 +0200

    Created TranslationsProvider component to wrap Next.js app

---
+++ app/_providers/translation/TranslationsProvider.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Resource } from "i18next";
+import { I18nextProvider } from "react-i18next";
+import { createInstance } from "i18next";
+import React from "react";
+import initTranslations from "@/app/i18n";
+
+const TranslationsProvider = ({
+  children,
+  locale,
+  namespaces,
+  resources,
+}: {
+  children: React.ReactNode;
+  locale: string;
+  namespaces: string[];
+  resources: Resource;
+}) => {
+  const i18n = createInstance();
+  initTranslations(locale, namespaces, i18n, resources);
+
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
+};
+
+export default TranslationsProvider;
