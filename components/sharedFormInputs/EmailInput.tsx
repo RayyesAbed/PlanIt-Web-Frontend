82a5c914c1e05c9e23a79418e5843b6d4b79ce99
RayyesAbed <abedray1567@gmail.com>
2025-10-05 12:44:00 +0200

    Added placeholder props to email input for translation

--- components/sharedFormInputs/EmailInput.tsx
+++ components/sharedFormInputs/EmailInput.tsx
@@ -1,11 +1,11 @@
 import { Input } from "@/components/ui/input";
 
-const EmailInput = () => {
+const EmailInput = ({ placeholder }: { placeholder: string }) => {
   return (
     <Input
       name="email"
       type="email"
-      placeholder="Email"
+      placeholder={placeholder}
       className="w-[300px] font-semibold"
       required={true}
     />
