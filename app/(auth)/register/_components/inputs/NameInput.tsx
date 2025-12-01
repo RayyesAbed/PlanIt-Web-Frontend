4ca223500d046b5024611c1f20c527189dd047b1
RayyesAbed <abdullarayes00@gmail.com>
2025-12-01 12:22:13 +0100

    Added value and onChange props to NameInput to use them in RegisterPageClient

--- app/(auth)/register/_components/inputs/NameInput.tsx
+++ app/(auth)/register/_components/inputs/NameInput.tsx
@@ -1,10 +1,21 @@
 import { Input } from "@/components/ui/input";
+import React from "react";
 
-const NameInput = ({ placeholder }: { placeholder: string }) => {
+const NameInput = ({
+  placeholder,
+  value,
+  onChange,
+}: {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => {
   return (
     <Input
       name="name"
       type="text"
+      value={value}
+      onChange={onChange}
       placeholder={placeholder}
       className="w-[300px] font-semibold"
       required={true}
