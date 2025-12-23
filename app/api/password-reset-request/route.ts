760a9238f5077d0f9e0787ce2899f47570504de9
RayyesAbed <abdullarayes00@gmail.com>
2025-12-23 12:19:38 +0100

    Added response to get result of fetch API for /auth/reset-password-request

--- app/api/password-reset-request/route.ts
+++ app/api/password-reset-request/route.ts
@@ -1,3 +1,13 @@
 export async function POST(requst: Request) {
   const body = await requst.json();
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/reset-password-request`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify(body),
+    }
+  );
 }
