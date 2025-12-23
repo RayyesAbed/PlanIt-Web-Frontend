2616e534d6c9003aad2355e190dbd746e49e69ca
RayyesAbed <abdullarayes00@gmail.com>
2025-12-23 12:42:20 +0100

    Added 400s error checking and returned success locale key if there are no errors

--- app/(auth)/login/lib/resetPasswordRequest.tsx
+++ app/(auth)/login/lib/resetPasswordRequest.tsx
@@ -1,6 +1,6 @@
 const resetPasswordRequest = async (email: string) => {
   try {
-    await fetch(`/api/password-reset-request`, {
+    const response = await fetch(`/api/password-reset-request`, {
       method: "POST",
       body: JSON.stringify({ email: email }),
       credentials: "include",
@@ -8,6 +8,19 @@ const resetPasswordRequest = async (email: string) => {
         "Content-Type": "application/json",
       },
     });
+
+    if (!response.ok) {
+      switch (response.status) {
+        case 400:
+          throw new Error("400Error");
+        case 401:
+          throw new Error("401Error");
+        case 429:
+          throw new Error("429Error");
+      }
+    }
+
+    return "200PasswordResetRequestMessage";
   } catch (error) {
     console.error("Password reset error:", error);
     throw new Error(error instanceof Error ? error.message : String(error));
