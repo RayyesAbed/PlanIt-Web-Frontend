e59f0dbee500a4c53cc6d6a5679f7a030d7c75e9
RayyesAbed <abdullarayes00@gmail.com>
2025-12-26 11:06:39 +0100

    Added pending, success, error and responseData useStates

--- app/_hooks/useAsyncAction.tsx
+++ app/_hooks/useAsyncAction.tsx
@@ -2,6 +2,11 @@ import { useState, useCallback } from "react";
 
 type AsyncAction<T> = () => Promise<T>;
 
-const useAsyncAction = () => {};
+const useAsyncAction = () => {
+  const [pending, setPending] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const [responseData, setResponseData] = useState<string | null>(null);
+};
 
 export default useAsyncAction;
