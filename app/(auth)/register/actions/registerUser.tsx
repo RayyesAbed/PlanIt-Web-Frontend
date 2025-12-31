145312f09cd6b5985eb455daf58593a25801580c
RayyesAbed <abdullarayes00@gmail.com>
2025-12-31 16:34:56 +0100

    Removed 'use server' directive in registerUser and returned Error locale strings to translate them in 'RegisterPageClient'

--- app/(auth)/register/actions/registerUser.tsx
+++ app/(auth)/register/actions/registerUser.tsx
@@ -1,37 +1,29 @@
-"use server";
 import RegisterCredentials from "../_types/RegisterCredentials";
-import translateFormStatus from "@/lib/translateFormStatus";
 
 const registerUser = async (
   registerCredentials: RegisterCredentials
 ): Promise<string> => {
-  const t = await translateFormStatus();
-
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register_request`,
-      {
-        method: "POST",
-        body: JSON.stringify(registerCredentials),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-
-    if (!response.ok) {
-      if (response.status == 400) {
-        throw new Error(t("emptyFieldsError"));
-      } else if (response.status == 429) {
-        throw new Error(t("429Error"));
-      }
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/register_request`,
+    {
+      method: "POST",
+      body: JSON.stringify(registerCredentials),
+      headers: {
+        "Content-Type": "application/json",
+      },
     }
+  );
 
-    return t("verificationEmail");
-  } catch (error) {
-    console.error("Register error:", error);
-    throw new Error(t("500Error"));
+  if (!response.ok) {
+    switch (response.status) {
+      case 400:
+        throw new Error("emptyFieldsError");
+      case 429:
+        throw new Error("429Error");
+    }
   }
+
+  return "201RegisterMessage";
 };
 
 export default registerUser;
