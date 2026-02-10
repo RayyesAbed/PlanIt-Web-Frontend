28e6fd4920aa24ede41d81aa107869237a0e1c4e
RayyesAbed <abdullarayes00@gmail.com>
2026-02-10 14:39:39 +0100

    Added SpinnerFeedback component if form is pending, otherwise returned register components

--- app/(auth)/register/RegisterPageClient.tsx
+++ app/(auth)/register/RegisterPageClient.tsx
@@ -18,6 +18,7 @@ import LanguageSelector from "@/app/(auth)/register/_components/inputs/LanguageS
 import { useState } from "react";
 import RegisterCredentials from "./_types/RegisterCredentials";
 import useAsyncFormAction from "@/app/_hooks/useAsyncFormAction";
+import SpinnerFeedback from "@/app/_components/shared/ui/spinnerFeedback";
 
 const RegisterPageClient = () => {
   const isDesktop = useMediaQuery("(min-width: 1280px)");
@@ -83,97 +84,105 @@ const RegisterPageClient = () => {
         <h1 className="font-bold text-xl my-5 text-center">
           {registerLocale.t("registerHeader")}
         </h1>
-        <section className="flex md:flex-row gap-5">
-          {/* Register in with Google or Apple */}
-          <GoogleButton
-            buttonAlt="Register with Google Icon"
-            ariaLabel="Register with Google"
+        {pending ? (
+          <SpinnerFeedback
+            text={formStatusMessageLocale.t("RegisteringStatus")}
           />
-          <AppleButton
-            buttonAlt="Register with Apple Icon"
-            ariaLabel="Register with Apple"
-          />
-        </section>
-        <section className="flex justify-center items-center my-5 w-full">
-          {/* The continue with bar */}
-          <div className="md:w-40 h-0.25 bg-gray-300"></div>
-          <span className="mx-2 text-center">
-            {registerLocale.t("continueText")}
-          </span>
-          <div className="md:w-40 h-0.25 bg-gray-300"></div>
-        </section>
-        <section className="flex-col items-center">
-          <form
-            onSubmit={handleRegisterSubmit}
-            className="flex flex-col items-center gap-3"
-          >
-            <Input
-              type="text"
-              value={registerCredentials?.name ?? ""}
-              onChange={(event) =>
-                updateRegisterCredentials("name", event.target.value)
-              }
-              placeholder={registerLocale.t("userName")}
-              className="w-[300px]"
-              pattern="[\p{L} ]+"
-              title={formStatusMessageLocale.t("InvalidNamePopup")}
-              required
-            />
-            <Input
-              type="email"
-              value={registerCredentials?.toBeConfirmedEmail ?? ""}
-              onChange={(event) =>
-                updateRegisterCredentials(
-                  "toBeConfirmedEmail",
-                  event.target.value,
-                )
-              }
-              placeholder={registerLocale.t("userEmail")}
-              className="w-[300px]"
-              required
-            />
-            <BirthDatePicker
-              value={registerCredentials?.birthDate ?? ""}
-              onChange={(birthDate) =>
-                updateRegisterCredentials("birthDate", birthDate)
-              }
-              placeholder={registerLocale.t("userBirthDate")}
-            />
-            <PasswordInput
-              value={registerCredentials.password}
-              onChange={(event) =>
-                updateRegisterCredentials("password", event.target.value)
-              }
-              placeholder={registerLocale.t("userPassword")}
-            />
-            <LanguageSelector
-              value={registerCredentials.language}
-              onSelect={(language) =>
-                updateRegisterCredentials("language", language)
-              }
-            />
-            <Button className="w-[150px] font-bold cursor-pointer">
-              {pending
-                ? "Registering..."
-                : registerLocale.t("registerButtonText")}
-            </Button>
-            <section className="flex flex-col gap-5 md:flex-row md:gap-10 underline">
-              <Link href="/login">
-                {registerLocale.t("existingUserLoginText")}
-              </Link>
+        ) : (
+          <>
+            <section className="flex md:flex-row gap-5">
+              {/* Register in with Google or Apple */}
+              <GoogleButton
+                buttonAlt="Register with Google Icon"
+                ariaLabel="Register with Google"
+              />
+              <AppleButton
+                buttonAlt="Register with Apple Icon"
+                ariaLabel="Register with Apple"
+              />
             </section>
-          </form>
-          {success && (
-            <p className="text-white p-2 mt-5 rounded-4xl text-center bg-green-900 ">
-              {responseData}
-            </p>
-          )}
-          {error && (
-            <p className="text-white p-2 mt-5 rounded-4xl text-center bg-red-900">
-              {responseData}
-            </p>
-          )}
-        </section>
+            <section className="flex justify-center items-center my-5 w-full">
+              {/* The continue with bar */}
+              <div className="md:w-40 h-0.25 bg-gray-300"></div>
+              <span className="mx-2 text-center">
+                {registerLocale.t("continueText")}
+              </span>
+              <div className="md:w-40 h-0.25 bg-gray-300"></div>
+            </section>
+            <section className="flex-col items-center">
+              <form
+                onSubmit={handleRegisterSubmit}
+                className="flex flex-col items-center gap-3"
+              >
+                <Input
+                  type="text"
+                  value={registerCredentials?.name ?? ""}
+                  onChange={(event) =>
+                    updateRegisterCredentials("name", event.target.value)
+                  }
+                  placeholder={registerLocale.t("userName")}
+                  className="w-[300px]"
+                  pattern="[\p{L} ]+"
+                  title={formStatusMessageLocale.t("InvalidNamePopup")}
+                  required
+                />
+                <Input
+                  type="email"
+                  value={registerCredentials?.toBeConfirmedEmail ?? ""}
+                  onChange={(event) =>
+                    updateRegisterCredentials(
+                      "toBeConfirmedEmail",
+                      event.target.value,
+                    )
+                  }
+                  placeholder={registerLocale.t("userEmail")}
+                  className="w-[300px]"
+                  required
+                />
+                <BirthDatePicker
+                  value={registerCredentials?.birthDate ?? ""}
+                  onChange={(birthDate) =>
+                    updateRegisterCredentials("birthDate", birthDate)
+                  }
+                  placeholder={registerLocale.t("userBirthDate")}
+                />
+                <PasswordInput
+                  value={registerCredentials.password}
+                  onChange={(event) =>
+                    updateRegisterCredentials("password", event.target.value)
+                  }
+                  placeholder={registerLocale.t("userPassword")}
+                />
+                <LanguageSelector
+                  value={registerCredentials.language}
+                  onSelect={(language) =>
+                    updateRegisterCredentials("language", language)
+                  }
+                />
+                <Button className="w-[150px] font-bold cursor-pointer">
+                  {pending
+                    ? "Registering..."
+                    : registerLocale.t("registerButtonText")}
+                </Button>
+                <section className="flex flex-col gap-5 md:flex-row md:gap-10 underline">
+                  <Link href="/login">
+                    {registerLocale.t("existingUserLoginText")}
+                  </Link>
+                </section>
+              </form>
+              {success && (
+                <p className="text-white p-2 mt-5 rounded-4xl text-center bg-green-900 ">
+                  {responseData}
+                </p>
+              )}
+              {error && (
+                <p className="text-white p-2 mt-5 rounded-4xl text-center bg-red-900">
+                  {responseData}
+                </p>
+              )}
+            </section>
+          </>
+        )}
       </section>
     </main>
   );
