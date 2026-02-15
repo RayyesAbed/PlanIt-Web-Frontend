01e1da690b4db4fd6733bf548ed159d54d1ec223
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 14:00:56 +0100

    Use Button component and add custom style to it and pass rest of props

--- app/_components/shared/ui/authButton.tsx
+++ app/_components/shared/ui/authButton.tsx
@@ -1,3 +1,12 @@
-function AuthButton() {}
+import { Button } from "./button";
+
+function AuthButton({ ...props }: React.ComponentProps<"button">) {
+  return (
+    <Button
+      className="w-[150px] cursor-pointer bg-[#242424] shadow-[0_20px_25px_-5px_rgba(0,0,0,0.2),inset_0_6px_16px_rgba(0,0,0,0.8)] active:scale-95 text-white"
+      {...props}
+    />
+  );
+}
 
 export default AuthButton;
