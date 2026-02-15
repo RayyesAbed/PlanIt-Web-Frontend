cec31f6663a086d0fe69137e82b39cea08d5d16b
RayyesAbed <abdullarayes00@gmail.com>
2026-02-15 14:00:04 +0100

    Remove custom styling in Button to add it in AuthButton

--- app/_components/shared/ui/button.tsx
+++ app/_components/shared/ui/button.tsx
@@ -50,7 +50,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={`${cn(buttonVariants({ variant, size, className }))} w-[150px] cursor-pointer bg-[#242424] shadow-[0_20px_25px_-5px_rgba(0,0,0,0.2),inset_0_6px_16px_rgba(0,0,0,0.8)] active:scale-95`}
+      className={`${cn(buttonVariants({ variant, size, className }))}`}
       {...props}
     />
   );
