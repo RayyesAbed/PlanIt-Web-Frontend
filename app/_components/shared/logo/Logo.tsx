b4a0cc7cdc3640f8b0181553cc6e520d770741b0
RayyesAbed <abdullarayes00@gmail.com>
2026-03-24 19:02:10 +0100

    feat(auth): dynamically set PlanIt daytime and dark alt texts

--- app/_components/shared/logo/Logo.tsx
+++ app/_components/shared/logo/Logo.tsx
@@ -3,21 +3,27 @@ import PlanItDaytimeLogo from "@/public/logo/PlanItDaytimeLogo.svg";
 import PlanItDarkLogo from "@/public/logo/PlanItDarkLogo.svg";
 import Link from "next/link";
 
-const Logo = () => {
+const Logo = ({
+  daytimeAlt,
+  darkAlt,
+}: {
+  daytimeAlt: string;
+  darkAlt: string;
+}) => {
   return (
     <Link href="/">
       <Image
         src={PlanItDaytimeLogo}
         width={160}
         height={160}
-        alt="PlanIt Daytime Logo"
+        alt={daytimeAlt}
         className="rounded-4xl block dark:hidden shadow-xl"
       />
       <Image
         src={PlanItDarkLogo}
         width={160}
         height={160}
-        alt="PlanIt Dark Logo"
+        alt={darkAlt}
         className="rounded-4xl hidden dark:block shadow-xl"
       />
     </Link>
