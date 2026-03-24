a43b8c0d9bf153c7fd14700b9ae3843986c43484
RayyesAbed <abdullarayes00@gmail.com>
2026-03-24 06:45:10 +0100

    refactor(auth): use Cypress config secrets for registering fake user

--- cypress/e2e/registration/register.cy.ts
+++ cypress/e2e/registration/register.cy.ts
@@ -3,8 +3,12 @@ describe("Registration", () => {
     cy.visit("http://localhost:5173/register");
 
     cy.get('input[name="name"]').type("Fake");
-    cy.get('input[name="email"]').type("fakeuser@test.com");
-    cy.get('input[name="password"]').dblclick().type("fakePassword123!");
+    cy.env(["fakeEmail"]).then(({ fakeEmail }) => {
+      cy.get("input[name='email']").type(fakeEmail);
+    });
+    cy.env(["fakePassword"]).then(({ fakePassword }) => {
+      cy.get('input[name="password"]').dblclick().type(fakePassword);
+    });
     cy.get('[data-testid="language-select"]').click();
     cy.get('[data-value="en"]').click();
 
