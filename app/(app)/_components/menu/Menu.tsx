a837f35f213535fc56f09b0bad7bd92479bdd1ec
RayyesAbed <abdullarayes00@gmail.com>
2026-03-27 19:19:42 +0100

    feat(app): add relative position and shadow effect to Menu component

--- app/(app)/_components/menu/Menu.tsx
+++ app/(app)/_components/menu/Menu.tsx
@@ -1,6 +1,6 @@
 const Menu = () => {
   return (
-    <ul className="bg-[rgba(203,203,203,0.82)] m-10 w-1/5 h-[calc(100vh-80px)] rounded-[45px]"></ul>
+    <ul className="bg-[rgba(203,203,203,0.82)] m-10 w-1/5 h-[calc(100vh-80px)] rounded-[45px] relative shadow-2xl"></ul>
   );
 };
 
