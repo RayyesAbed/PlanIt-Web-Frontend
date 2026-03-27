5fd185ce423c3004fd067449373899106051630d
RayyesAbed <abdullarayes00@gmail.com>
2026-03-27 19:22:29 +0100

    feat(app): add outsourced user profile data menu item in Menu component

--- app/(app)/_components/menu/Menu.tsx
+++ app/(app)/_components/menu/Menu.tsx
@@ -1,6 +1,10 @@
+import ProfileMenuItem from "./ProfileMenuItem";
+
 const Menu = () => {
   return (
-    <ul className="bg-[rgba(203,203,203,0.82)] m-10 w-1/5 h-[calc(100vh-80px)] rounded-[45px] relative shadow-2xl"></ul>
+    <ul className="bg-[rgba(203,203,203,0.82)] m-10 w-1/5 h-[calc(100vh-80px)] rounded-[45px] relative shadow-2xl">
+      <ProfileMenuItem />
+    </ul>
   );
 };
 
