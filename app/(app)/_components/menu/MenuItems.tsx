3b702e938f7f6274e001104d33ff9ddcb8722211
RayyesAbed <abdullarayes00@gmail.com>
2026-03-27 19:49:46 +0100

    feat(app): add 'path' property to each menu item

--- app/(app)/_components/menu/MenuItems.tsx
+++ app/(app)/_components/menu/MenuItems.tsx
@@ -10,26 +10,32 @@ import {
 const MenuItems = [
   {
     locale: "tasks",
+    path: "/tasks",
     icon: <SquareCheck />,
   },
   {
     locale: "performance",
+    path: "/performance",
     icon: <Gauge />,
   },
   {
     locale: "story",
+    path: "/story",
     icon: <BookText />,
   },
   {
     locale: "store",
+    path: "/store",
     icon: <Store />,
   },
   {
     locale: "calendar",
+    path: "/calendar",
     icon: <Calendar />,
   },
   {
     locale: "settings",
+    path: "/settings",
     icon: <Settings />,
   },
 ];
