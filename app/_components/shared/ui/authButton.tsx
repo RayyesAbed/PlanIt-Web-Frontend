76ff6b667a51dd1ca576f4b4305ba7d5ccba9668
RayyesAbed <abdullarayes00@gmail.com>
2026-03-27 18:15:53 +0100

    fix(auth): fix auth button style in dark mode

--- app/_components/shared/ui/authButton.tsx
+++ app/_components/shared/ui/authButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "./button";
 function AuthButton({ ...props }: React.ComponentProps<"button">) {
   return (
     <Button
-      className="w-[150px] cursor-pointer bg-[#242424] shadow-[0_20px_25px_-5px_rgba(0,0,0,0.2),inset_0_6px_16px_rgba(0,0,0,0.8)] active:scale-95 text-white"
+      className="w-[150px] cursor-pointer bg-[#242424] shadow-[0_20px_25px_-5px_rgba(0,0,0,0.2),inset_0_6px_16px_rgba(0,0,0,0.8)] active:scale-95 text-white dark:bg-white dark:shadow-none dark:text-[#242424]"
       {...props}
     />
   );
