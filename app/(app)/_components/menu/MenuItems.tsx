e8da90bed334df57c04ee4a22302ef51a2b1a530
RayyesAbed <abdullarayes00@gmail.com>
2026-03-27 19:01:04 +0100

    feat(app): add menu items' locales and icons

--- app/(app)/_components/menu/MenuItems.tsx
+++ app/(app)/_components/menu/MenuItems.tsx
@@ -1,3 +1,37 @@
-const MenuItems = [];
+import {
+  BookText,
+  Calendar,
+  Gauge,
+  Settings,
+  SquareCheck,
+  Store,
+} from "lucide-react";
+
+const MenuItems = [
+  {
+    locale: "tasks",
+    icon: <SquareCheck />,
+  },
+  {
+    locale: "performance",
+    icon: <Gauge />,
+  },
+  {
+    locale: "story",
+    icon: <BookText />,
+  },
+  {
+    locale: "store",
+    icon: <Store />,
+  },
+  {
+    locale: "calendar",
+    icon: <Calendar />,
+  },
+  {
+    locale: "settings",
+    icon: <Settings />,
+  },
+];
 
 export default MenuItems;
