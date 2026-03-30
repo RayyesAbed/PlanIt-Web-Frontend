7dcd90c70cc87ce1e4446cde1bc876c3463a2ce8
RayyesAbed <abdullarayes00@gmail.com>
2026-03-30 17:18:33 +0200

    feat(app): control user profile component's appearance depending on screen's width

--- app/(app)/_components/menu/ProfileMenuItem.tsx
+++ app/(app)/_components/menu/ProfileMenuItem.tsx
@@ -1,6 +1,6 @@
 const ProfileMenuItem = () => {
   return (
-    <li className="bg-[rgba(251,251,251,0.82)] dark:bg-[rgba(50,50,50,0.82)] dark:text-white m-4 rounded-[45px] h-16 w-[calc(100%-32px)] flex justify-center items-center gap-5 absolute">
+    <li className="bg-[rgba(251,251,251,0.82)] dark:bg-[rgba(50,50,50,0.82)] dark:text-white m-4 rounded-[45px] h-16 w-[calc(100%-32px)] hidden lg:flex justify-center items-center gap-5 absolute">
       <h1>IMG</h1>
       <section>
         <p>NAME</p>
