70710c057394f049f6e38a0121788cce36b005a2
RayyesAbed <abdullarayes00@gmail.com>
2026-03-30 17:38:39 +0200

    feat(app): adjust menu item's width depending on screen's width

--- app/(app)/_components/menu/Menu.tsx
+++ app/(app)/_components/menu/Menu.tsx
@@ -18,7 +18,7 @@ const Menu = () => {
             <Link
               key={index}
               href={menuItem.path}
-              className={`font-medium m-4 rounded-[45px] h-16  flex justify-center items-center gap-5 hover:bg-[#242424] hover:text-white dark:hover:bg-[rgba(222,222,222,0.82)] dark:hover:text-[#242424] transition-colors ${pathName.includes(menuItem.path) ? "bg-[#242424] text-white dark:bg-[rgba(222,222,222,0.82)] dark:text-[#242424]" : "bg-white text-[#242424] dark:bg-[rgba(50,50,50,0.82)] dark:text-white"}`}
+              className={`font-medium m-4 rounded-[45px] h-16 flex justify-center items-center gap-5 hover:bg-[#242424] hover:text-white dark:hover:bg-[rgba(222,222,222,0.82)] dark:hover:text-[#242424] transition-colors w-20 lg:w-auto ${pathName.includes(menuItem.path) ? "bg-[#242424] text-white dark:bg-[rgba(222,222,222,0.82)] dark:text-[#242424]" : "bg-white text-[#242424] dark:bg-[rgba(50,50,50,0.82)] dark:text-white"}`}
             >
               <span>{menuItem.icon}</span>
               <span className="hidden lg:block">
