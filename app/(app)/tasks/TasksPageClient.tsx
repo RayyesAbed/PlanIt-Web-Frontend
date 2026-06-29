9b82805a595dc01ae21d19728691ad6e54fce9fb
RayyesAbed <abdullarayes00@gmail.com>
2026-06-29 17:18:49 +0200

    feat(app): wrap ThemeToggle component in section

--- app/(app)/tasks/TasksPageClient.tsx
+++ app/(app)/tasks/TasksPageClient.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import ThemeToggle from "@/app/_components/themeToggle/ThemeToggle";
 import Menu from "../_components/menu/Menu";
 
 const TasksPageClient = () => {
   return (
     <main className="flex">
       <Menu />
+      <section>
+        <ThemeToggle />
+      </section>
     </main>
   );
 };
