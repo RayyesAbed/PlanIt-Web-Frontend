ec4faa279203c970a3a04f623981249a10856678
RayyesAbed <abdullarayes00@gmail.com>
2026-06-29 17:52:55 +0200

    feat(app): style Tasks component and heading in dark mode

--- app/(app)/tasks/TasksPageClient.tsx
+++ app/(app)/tasks/TasksPageClient.tsx
@@ -5,13 +5,15 @@ import Menu from "../_components/menu/Menu";
 
 const TasksPageClient = () => {
   return (
-    <main className="flex bg-[#EDEDED]">
+    <main className="flex bg-[#EDEDED] dark:bg-[#393838]">
       <Menu />
       <section className="flex-2/3 flex relative">
         <ThemeToggle />
         <section>
           <section>
-            <h1 className="text-[#393838] text-[36px] font-bold">Tasks</h1>
+            <h1 className="text-[#393838] dark:text-white text-[36px] font-bold">
+              Tasks
+            </h1>
           </section>
         </section>
       </section>
