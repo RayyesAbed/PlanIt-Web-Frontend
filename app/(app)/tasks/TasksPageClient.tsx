1d29166fdf44cdc2341e7337f7af2ff1d64320a1
RayyesAbed <abdullarayes00@gmail.com>
2026-06-29 17:17:11 +0200

    feat(app): add Menu component in flex container

--- app/(app)/tasks/TasksPageClient.tsx
+++ app/(app)/tasks/TasksPageClient.tsx
@@ -1,5 +1,13 @@
 "use client";
 
-const TasksPageClient = () => {};
+import Menu from "../_components/menu/Menu";
+
+const TasksPageClient = () => {
+  return (
+    <main className="flex">
+      <Menu />
+    </main>
+  );
+};
 
 export default TasksPageClient;
