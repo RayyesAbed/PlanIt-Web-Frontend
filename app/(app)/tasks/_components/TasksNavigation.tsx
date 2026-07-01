6e916bd5d2ee71bc597d3f325b69c8f9465172c4
RayyesAbed <abdullarayes00@gmail.com>
2026-07-01 16:05:13 +0200

    feat(app): conditionally style task navigation item background color and add transition and scaling animation

--- app/(app)/tasks/_components/TasksNavigation.tsx
+++ app/(app)/tasks/_components/TasksNavigation.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const TaskNavigationItem = ({ text, href }: { text: string; href: string }) => {
+  const pathname = usePathname();
+
   return (
     <Link
       href={href}
-      className="flex-1/4 text-center bg-[#3E3E3E] text-white font-semibold rounded-[45px] py-2 mx-2 shadow-md inset-shadow-sm"
+      className={`flex-1/4 text-center ${pathname == href ? "bg-[#3E3E3E]" : "bg-[#ABA9A9]"} hover:bg-[#3E3E3E] transition-all text-white font-semibold rounded-[45px] py-2 mx-2 shadow-md active:scale-97`}
     >
       <li>{text}</li>
     </Link>
