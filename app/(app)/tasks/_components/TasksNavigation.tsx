5a0b58d4198dfa057fec2a8fa4fc856c5701ed65
RayyesAbed <abdullarayes00@gmail.com>
2026-07-01 15:57:20 +0200

    feat(app): wrap task navigation item in NextJS Link component and pass href to it

--- app/(app)/tasks/_components/TasksNavigation.tsx
+++ app/(app)/tasks/_components/TasksNavigation.tsx
@@ -1,14 +1,23 @@
-const TaskNavigationItem = ({ text }: { text: string }) => {
-  return <li className="flex-1/4">{text}</li>;
+import Link from "next/link";
+
+const TaskNavigationItem = ({ text, href }: { text: string; href: string }) => {
+  return (
+    <Link
+      href={href}
+      className="flex-1/4 text-center bg-[#3E3E3E] text-white font-semibold rounded-[45px] py-2 mx-2 shadow-md inset-shadow-sm"
+    >
+      <li>{text}</li>
+    </Link>
+  );
 };
 
 const TasksNavigation = () => {
   return (
     <ul className="flex justify-center">
-      <TaskNavigationItem text="Today" />
-      <TaskNavigationItem text="Upcoming" />
-      <TaskNavigationItem text="Completed" />
-      <TaskNavigationItem text="Overdue" />
+      <TaskNavigationItem text="Today" href="#" />
+      <TaskNavigationItem text="Upcoming" href="#" />
+      <TaskNavigationItem text="Completed" href="#" />
+      <TaskNavigationItem text="Overdue" href="#" />
     </ul>
   );
 };
