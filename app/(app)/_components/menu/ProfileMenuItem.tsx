c6a1eae9c8688326753d45967298db8654fc1b2e
RayyesAbed <abdullarayes00@gmail.com>
2026-07-14 07:38:34 +0200

    feat(app): get user's data and display them in Profile component

--- app/(app)/_components/menu/ProfileMenuItem.tsx
+++ app/(app)/_components/menu/ProfileMenuItem.tsx
@@ -1,12 +1,28 @@
+import { GET_USER_DATA } from "@/app/_graphql/typeDefs";
+import { GetUserDataQuery } from "@/app/_types/Auth";
+import { useQuery } from "@apollo/client/react";
+import Image from "next/image";
+import EmptyProfileImage from "@/public/emptyProfilePicture.svg";
+
 const ProfileMenuItem = () => {
+  const { data } = useQuery<GetUserDataQuery>(GET_USER_DATA);
+
   return (
     <li className="bg-[rgba(251,251,251,0.82)] dark:bg-[rgba(50,50,50,0.82)] dark:text-white m-4 rounded-[45px] h-16 w-[calc(100%-32px)] hidden lg:flex justify-center items-center gap-5 absolute">
-      <h1>IMG</h1>
+      <h1>
+        <Image
+          src={data?.getUserData.picture ?? EmptyProfileImage}
+          alt="Profile photo"
+          width={60}
+          height={60}
+          className="rounded-[45px]"
+        />
+      </h1>
       <section>
-        <p>NAME</p>
+        <p>{data?.getUserData.name}</p>
         <p>
-          <span>XP</span>
-          <span>PLAN</span>
+          <span>{data?.getUserData.points} XP</span>
+          <span>{data?.getUserData.subscription.name}</span>
         </p>
       </section>
     </li>
