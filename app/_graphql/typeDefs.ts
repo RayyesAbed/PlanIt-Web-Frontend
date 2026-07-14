9c2324f343e79113c798f8ed91c624e8cf47fcf6
RayyesAbed <abdullarayes00@gmail.com>
2026-07-14 06:33:53 +0200

    feat(app): add GraphQL user data query

---
+++ app/_graphql/typeDefs.ts
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER_DATA = gql`
+  query {
+    getUserData {
+      name
+      confirmedEmail
+      birthDate
+      preferredLanguage
+      points
+    }
+  }
+`;
